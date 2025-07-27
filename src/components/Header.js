451f5d172731fed23df06ff1fa352d29901f65aa
Itu Bhawal <bhawalitu23@gmail.com>
2025-07-28 00:22:41 +0530

    API integration & Shimmer UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+  // let btnName = "Login";
+
+  console.log("Header Rendered");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +19,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+              // btnName = "Logout"; // UI will not get updated as the comp doesn't re-renders with normal JS vars
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
